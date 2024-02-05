d9d72d96a6511dec70b14ce977b01797df9656cd
Hjordten <andr314e@stud.kea.dk>
2024-02-05 11:10:50 +0100

    .

--- ses2/exercises/list2.py
+++ ses2/exercises/list2.py
@@ -9,11 +9,13 @@
 # all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns (1, 2, 3). You may create a new list or
 # modify the passed in list.
-
-
 def remove_adjacent(nums):
-    # +++your code here+++
-    return
+        new_list = []
+        for item in nums:
+             if item not in new_list:
+                  new_list.append(item)
+        new_list = tuple(new_list)          
+        return new_list
     
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -21,8 +23,7 @@ def remove_adjacent(nums):
 # Ideally, the solution should work in "linear" time, making a single
 # pass of both lists.
 def linear_merge(list1, list2):
-    # +++your code here+++
-   return
+    return sorted(list1 + list2)
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
